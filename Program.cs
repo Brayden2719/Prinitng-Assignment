b7854fc033df459bb37e78123780034528bfc99d
339828725 <339828725@2260I-L29-019>
2026-02-04 13:12:12 -0500

    Done

--- Program.cs
+++ Program.cs
@@ -4,10 +4,21 @@
     {
         static void Main(string[] args)
         {
-            // Brayden
+            // Brayden Brockington
+            // The double Bs are my initials
             Console.WriteLine("Hello, my name is Brayden Brockington");
             Console.WriteLine();
-            Console.WriteLine("Comments are used to help yourself remember things, and let others know what is on your coding);
+            Console.WriteLine("Comments are used to help yourself remember things, and let others know what is on your coding");
+            Console.WriteLine();
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("BBBB     BBBB");
+            Console.WriteLine("B   B    B   B");
+            Console.WriteLine("B   B    B   B");
+            Console.WriteLine("BBBB     BBBB");
+            Console.WriteLine("B   B    B   B");
+            Console.WriteLine("B   B    B   B");
+            Console.WriteLine("BBBB     BBBB");
+           
 
             
 
