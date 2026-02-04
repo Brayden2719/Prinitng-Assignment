9b6f65d497de3c09b7215acc71aa324b0fab3a80
339828725 <339828725@2260I-L29-019>
2026-02-04 12:35:28 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Prinitng_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, my name is Brayden Brockington");
+            Console.WriteLine();
+            Console.WriteLine("Comments are used to help yourself remember things, and let others know what is on your coding);
+
+            
+
+        }
+    }
+}
