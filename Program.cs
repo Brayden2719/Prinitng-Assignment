4f499d6f9d02316561419cb347674ee7e1478473
339828725 <339828725@2260I-L29-019>
2026-02-04 12:37:06 -0500

    add a comment

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            // Brayden
             Console.WriteLine("Hello, my name is Brayden Brockington");
             Console.WriteLine();
             Console.WriteLine("Comments are used to help yourself remember things, and let others know what is on your coding);
